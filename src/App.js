254e1a28a6ee4f646aab977aa28ce7c5393ebfff
Alibek Khojabekov <alibek.khojabekov@nu.edu.kz>
2022-02-23 22:18:37 +0600

    simple routing setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router,Link, Route, Routes } from "react-router-dom";
+
+import Login from './pages/Login';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5 }}>
+          Home
+        </Link>
+        <Link to="/login" style={{ padding: 5 }}>
+          Login
+        </Link>
+      </nav>
+  {/* Rest of the code remains same */}
+
+    <Routes>
+        <Route path="/login" element={<Login/>}/>
+    </Routes>
+</Router>
+
+
+    
   );
 }
 
---
+++ src/components/input/Input.js
@@ -0,0 +1,8 @@
+import './Input.css'
+function Input(name){
+    return (
+        <input>{{name}}</input>
+    )
+}
+
+export default Input;
