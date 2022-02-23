34e6f8bf27d515d9469c4f3897743e78f572f3c6
Alibek Khojabekov <alibek.khojabekov@nu.edu.kz>
2022-02-23 22:18:06 +0600

    loading components

---
+++ src/components/button/Button.js
@@ -0,0 +1,9 @@
+import './Button.css'
+function Button(props) {
+    return (
+        <button className="btn"> Register</button>
+    )
+
+}
+
+export default Button;
---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import Button from '../components/button/Button';
+function Login (){
+    return(
+        <section className="form">
+        <h1>Testing grid</h1>
+        <Button props='#1A4F8B'/>
+        </section>
+    )
+}
+
+export default Login;
