b880930e2b747f9eac3969bbe6f47791c2b5092c
Alibek Khojabekov <alibek.khojabekov@nu.edu.kz>
2022-02-24 03:31:39 +0600

    final: fixing position

--- src/pages/Upgrade.js
+++ src/pages/Upgrade.js
@@ -7,12 +7,14 @@ function Upgrade(){
             <h1 className="form__title">Get Pro Account</h1>
             <img src={require('../assets/images/undraw_online_payments_luau 1.png')} alt="Upgrade-illustration" className="image-container"/>
             <CardInput placeholder='Card Number' type="number"/>
+            <div>
             <div style={{display: 'flex', justifyContent: 'space-between'}}>
             <CardInput placeholder='MM/YY' width="165px" right= "7px"/>
             <CardInput placeholder='CVV' width="165px" type="number" left=" 7px"/>
             </div>
             <p className="question">What’s this?</p>
             <CardInput placeholder='ZIP/Postal Code'/>
+            </div>
             <Button title="Upgrade" backgroundColor="#BF1541"/>
         </section>
     )
